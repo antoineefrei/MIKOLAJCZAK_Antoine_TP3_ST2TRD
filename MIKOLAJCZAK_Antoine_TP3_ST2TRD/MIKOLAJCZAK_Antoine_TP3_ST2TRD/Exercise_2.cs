6233a5f6e423c3cf0af1668a34fcb50ad3a8fec5
antoineefrei <55093994+antoineefrei@users.noreply.github.com>
2022-01-19 22:43:45 +0100

    Add files via upload

---
+++ MIKOLAJCZAK_Antoine_TP3_ST2TRD/MIKOLAJCZAK_Antoine_TP3_ST2TRD/Exercise_2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace MIKOLAJCZAK_Antoine_TP3_ST2TRD
+{
+    public class Exercise_2
+    {
+        Mutex mut = new Mutex();
+        public void fct(string x, int time, int timer)
+        {
+            while (timer != 0)
+            {
+                mut.WaitOne();
+                Console.Write(x);
+                mut.ReleaseMutex();
+                Thread.Sleep(time);
+                timer -= 1;
+                
+            }
+            
+        }
+
+
+        public void launch()
+        {
+            
+            Console.WriteLine("///////////// Exercise 2 \\\\\\\\\\\\\\\\");
+            var t1 = new Thread( ()=> fct(" ", 50,200));
+            var t2 = new Thread( ()=> fct("*", 40,275));
+            var t3 = new Thread( ()=> fct("°", 20,450));
+            
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            t1.Join();
+            t2.Join();
+            t3.Join();
+        }
+        
+    }
+}
---
+++ MIKOLAJCZAK_Antoine_TP3_ST2TRD/MIKOLAJCZAK_Antoine_TP3_ST2TRD/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using TP3;
+
+
+namespace MIKOLAJCZAK_Antoine_TP3_ST2TRD
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //  Display the title of the oldest movie
+
+            var Movie = new MovieCollection();
+            Exercise_2 ex2 = new Exercise_2();
+            
+
+            Movie.Launch();
+            ex2.launch();
+
+
+
+
+
+
+            
+            
+            
+
+            
+            
+            
+            
+            
+            
+            
+        }
+        
+        
+    }
+}
